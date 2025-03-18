28ba40775f73969fb8f32cce30d64849ab3f8b2a
Abhishek Sharma <abhisheksharma879150@gmail.com>
2025-03-19 02:17:15 +0530

    -

---
+++ src/Components/General/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default NavBar
